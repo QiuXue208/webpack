63eb19b2b1a41527381cfef70084663bd1ab1fa1
qiuxue <1309571292@qq.com>
2023-02-01 22:48:16 +0800

    feat: 实现一个简单的插件

---
+++ loader-demo/demo-webpack-plugin.js
@@ -0,0 +1,23 @@
+// 打包的时候自动生成一个md文档
+class DemoWebpackPlugin {
+  constructor() {
+    console.log('plugin init')
+  }
+  apply(compiler) {
+    // compiler 是一个webpack实例
+    compiler.hooks.compile.tap('DemoWebpackPlugin', compilation => console.log(compilation))
+    compiler.hooks.emit.tapAsync('DemoWebpackPlugin', (compilation, fn) => {
+      compilation.assets['index.md'] = {
+        source: function() {
+          return 'this is a demo for plugin'
+        },
+        size: function() {
+          return 25
+        }
+      }
+      fn()
+    })
+  }
+}
+
+module.exports = DemoWebpackPlugin
